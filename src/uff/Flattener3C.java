cd9508dd549abfaa24ad649d4198a04859a76a6d
xinwucwp <xinwucwp@gmail.com>
2014-09-27 22:14:11 -0600

    update

--- src/uff/Flattener3C.java
+++ src/uff/Flattener3C.java
@@ -696,7 +696,7 @@ public class Flattener3C {
       for (int i1=1,i1m=0; i1<n1; ++i1,++i1m) {
         float wpi = (wp!=null)?wp[i3][i2][i1]:1.0f;
         if(wpi<0.05f) {wpi=0.05f;}
-        float w1i = 0.0f;
+        float w1i = 0.002f;
         //float w1i = w1[i3][i2][i1];
         float p2i = p2[i3][i2][i1];
         float p3i = p3[i3][i2][i1];
