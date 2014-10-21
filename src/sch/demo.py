851e0558baf47043d44e75ddbc31d093504c2d12
xinwucwp <xinwucwp@gmail.com>
2014-10-20 20:10:33 -0600

    update

--- src/sch/demo.py
+++ src/sch/demo.py
@@ -60,7 +60,7 @@ maxThrow = 20.0
 pngDir = "../../../png/sch/"
 
 # We can avoid most computations entirely be setting plotOnly to True.
-plotOnly = True
+plotOnly = False
 
 # Processing begins here. When experimenting with one part of this demo, we
 # can comment out other parts that have already written results to files.
