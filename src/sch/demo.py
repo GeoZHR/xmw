075ad354b7e54dd4fb8819d1e71d86c364495e74
xinwucwp <xinwucwp@gmail.com>
2014-10-22 23:51:13 -0600

    update

--- src/sch/demo.py
+++ src/sch/demo.py
@@ -340,7 +340,6 @@ def goUnfaultc():
     cfs = ConstraintsFromFaults(skins,ep)
     wp = pow(ep,2.0)
     cs = cfs.getWeightsAndConstraints(wp,cp)
-    '''
     fm = cfs.getFaultMap()
     u1 = fillfloat(1.0,n1,n2,n3)
     u2 = fillfloat(0.0,n1,n2,n3)
@@ -361,11 +360,9 @@ def goUnfaultc():
     ft = readImage(ftcfile)
     cp = readImage(cpfile)
     gx = readImage(gxfile)
-  '''
   hmin,hmax,hmap = -3.0,3.0,ColorMap.GRAY
 
   plot3(cp,cmin=hmin,cmax=hmax,cmap=hmap,clab="ControlPointsM",png="cp")
-  '''
   plot3(ft,cmin=hmin,cmax=hmax,cmap=hmap,clab="UnfaultC",png="ft")
   plot3(gx,r1,cmin=-5.0,cmax=8.0,cmap=jetFill(0.3),
         clab="Vertical shift (samples)",png="gxs1i")
@@ -373,7 +370,6 @@ def goUnfaultc():
         clab="Inline shift (samples)",png="gxs2i")
   plot3(gx,r3,cmin=-1.0,cmax=1.0,cmap=jetFill(0.3),
         clab="Crossline shift (samples)",png="gxs3i")
-  '''
 
 
 def goUnfoldc():
