c6520f0de617254c419ce1d042bb2948e303b941
xinwucwp <xinwucwp@gmail.com>
2015-05-26 08:28:12 -0600

    update

--- src/uff/demo.py
+++ src/uff/demo.py
@@ -69,14 +69,13 @@ maxThrow =  25.0
 
 # Directory for saved png images. If None, png images will not be saved;
 # otherwise, must create the specified directory before running this script.
-#pngDir = None
-pngDir = "../../../png/uff/"
+pngDir = None
+#pngDir = "../../../png/uff/"
 plotOnly = False
 
 # Processing begins here. When experimenting with one part of this demo, we
 # can comment out earlier parts that have already written results to files.
 def main(args):
-  '''
   goFakeData()
   goSlopes()
   goScan()
@@ -85,7 +84,6 @@ def main(args):
   goReSkin()
   goSmooth()
   goSlip()
-  '''
   goUnfaultS()
   #goUnfaultC()
   #go2dFault()
