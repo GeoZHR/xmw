0bddd98b003fad7fd8ec1b301018d7abd873720d
xinwucwp <xinwucwp@gmail.com>
2015-12-19 23:37:27 -0600

    update

--- src/ad/FastExplicitDiffusion.java
+++ src/ad/FastExplicitDiffusion.java
@@ -101,7 +101,8 @@ public class FastExplicitDiffusion {
     for (int m=0; m<_m; ++m) {
       trace("m="+m);
     for (int ic=0; ic<nc; ++ic) {
-      applyLaplacian(et,-ts[ic],wp,copy(gx),gx);
+      //applyLaplacian(et,-ts[ic],wp,copy(gx),gx);
+      applyParallel(et,-ts[ic],wp,copy(gx),gx);
     }}
     return gx;
   }
@@ -174,6 +175,67 @@ public class FastExplicitDiffusion {
       }
     }
   }
+
+  private void applyParallel(
+    final Tensors3 d, final float c, final float[][][] s, 
+    final float[][][] x, final float[][][] y) 
+  {
+    int i3start = 1; 
+    final int i3step = 2; 
+    final int i3stop = x.length;
+    for (int i3pass=0; i3pass<i3step; ++i3pass,++i3start) {
+      Parallel.loop(i3start,i3stop,i3step,new Parallel.LoopInt() {
+        public void compute(int i3) {
+          apply22(i3,d,c,s,x,y);
+        }
+      });
+    }
+  }
+
+  private void apply22(
+    int i3, Tensors3 d, float c, float[][][] s, float[][][] x, float[][][] y) 
+  {
+    c *= 0.0625f;
+    int n1 = x[0][0].length;
+    int n2 = x[0].length;
+    float[] di = new float[6];
+    for (int i2=1; i2<n2; ++i2) {
+      float[] x00 = x[i3  ][i2  ];
+      float[] x0m = x[i3  ][i2-1];
+      float[] xm0 = x[i3-1][i2  ];
+      float[] xmm = x[i3-1][i2-1];
+      float[] y00 = y[i3  ][i2  ];
+      float[] y0m = y[i3  ][i2-1];
+      float[] ym0 = y[i3-1][i2  ];
+      float[] ymm = y[i3-1][i2-1];
+      for (int i1=1,m1=0; i1<n1; ++i1,++m1) {
+        d.getTensor(i1,i2,i3,di);
+        float csi = (s!=null)?c*s[i3][i2][i1]:c;
+        float d11 = di[0]*csi;
+        float d12 = di[1]*csi;
+        float d13 = di[2]*csi;
+        float d22 = di[3]*csi;
+        float d23 = di[4]*csi;
+        float d33 = di[5]*csi;
+        float xa = x00[i1]-xmm[m1];
+        float xb = x00[m1]-xmm[i1];
+        float xc = x0m[i1]-xm0[m1];
+        float xd = xm0[i1]-x0m[m1];
+        float x1 = xa-xb+xc+xd;
+        float x2 = xa+xb-xc+xd;
+        float x3 = xa+xb+xc-xd;
+        float y1 = d11*x1+d12*x2+d13*x3;
+        float y2 = d12*x1+d22*x2+d23*x3;
+        float y3 = d13*x1+d23*x2+d33*x3;
+        float ya = y1+y2+y3; y00[i1] += ya; ymm[m1] -= ya;
+        float yb = y1-y2+y3; y0m[i1] += yb; ym0[m1] -= yb;
+        float yc = y1+y2-y3; ym0[i1] += yc; y0m[m1] -= yc;
+        float yd = y1-y2-y3; ymm[i1] += yd; y00[m1] -= yd;
+      }
+    }
+  }
+
+
 
   private static void applyLaplacian(
     final EigenTensors3 d, final float c, 
