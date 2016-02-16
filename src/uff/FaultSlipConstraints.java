03ce70b258424ce21f66d2be6de6d193927f1c98
xinwucwp <xinwucwp@gmail.com>
2016-02-16 16:58:05 -0600

    update

--- src/uff/FaultSlipConstraints.java
+++ src/uff/FaultSlipConstraints.java
@@ -231,8 +231,8 @@ public class FaultSlipConstraints {
       float x3max = min(max(x3a),n3-1);
       SibsonInterp s1i = new SibsonInterp(s1a,x1a,x2a,x3a);
       s1i.setBounds(x1min,x1max,x2min,x2max,x3min,x3max);
-      for (int ic=0; ic<nc; ++ic) {
-        System.out.println("ic="+ic);
+      for (int ic=1; ic<nc; ++ic) {
+        //System.out.println("ic="+ic);
         FaultCell cell = cells[ic];
         float s1 = cell.getS1();
         float s2 = cell.getS2();
