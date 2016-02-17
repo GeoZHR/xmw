66c429b9f91bb2623f983563fd080bdfbb60b5b0
xinwucwp <xinwucwp@gmail.com>
2016-02-16 20:22:47 -0600

    update

--- src/beg/demo.py
+++ src/beg/demo.py
@@ -69,7 +69,7 @@ maxThrow = 60.0
 pngDir = None
 #pngDir = "../../../png/beg/hongliu/"
 #pngDir = "../../../png/beg/bp/sub1/"
-plotOnly = True
+plotOnly = False
 
 # Processing begins here. When experimenting with one part of this demo, we
 # can comment out earlier parts that have already written results to files.
