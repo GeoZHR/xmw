479caf688d9ca9f355d1192fbc84fe018a4faab7
xinwucwp <xinwucwp@gmail.com>
2016-02-28 08:02:25 -0600

    update

--- src/beg/nathan.py
+++ src/beg/nathan.py
@@ -81,8 +81,8 @@ plotOnly = False
 # Processing begins here. When experimenting with one part of this demo, we
 # can comment out earlier parts that have already written results to files.
 def main(args):
-  goSlopes()
-  #goScan()
+  #goSlopes()
+  goScan()
   #goThin()
   #goSkin()
   #goSkinTv()
