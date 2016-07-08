c45d6795d74f657aba1ca7e291f533e9a21341ab
xinwucwp <xinwucwp@gmail.com>
2016-07-08 10:26:18 -0500

    update

--- src/poseidon/fakeDemo.py
+++ src/poseidon/fakeDemo.py
@@ -47,9 +47,9 @@ plotOnly = False
 # Processing begins here. When experimenting with one part of this demo, we
 # can comment out earlier parts that have already written results to files.
 def main(args):
-  #goFakeData()
-  #goSlopes()
-  #goSingleHorizon()
+  goFakeData()
+  goSlopes()
+  goSingleHorizon()
   goHorizonVolume()
 
 def goFakeData():
