088266bb1dd5355ee2a7818f5ee0badd3af6e6f0
xinwucwp <xinwucwp@gmail.com>
2016-08-10 16:39:45 -0500

    update

--- src/sso/sso3.py
+++ src/sso/sso3.py
@@ -42,14 +42,14 @@ n1,n2,n3 = 400,700,800
 s1 = Sampling(n1,d1,f1)
 s2 = Sampling(n2,d2,f2)
 s3 = Sampling(n3,d3,f3)
-plotOnly = False
+plotOnly = True
 
 def main(args):
   #goLof()
-  goLoe()
+  #goLoe()
   #goChannel()
   #goSmoothSL()
-  #goSmoothSS()
+  goSmoothSS()
   #goSmoothC()
   #goFirstLook()
 def goFirstLook():
