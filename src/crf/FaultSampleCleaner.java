d0cc66ffaebf419cf7703c64311c081ef3932d28
xinwucwp <xinwucwp@gmail.com>
2017-02-07 15:19:33 -0600

    update

--- src/crf/FaultSampleCleaner.java
+++ src/crf/FaultSampleCleaner.java
@@ -20,10 +20,9 @@ import mef.*;
 public class FaultSampleCleaner {
 
   public void recomputeLikelihoods(
-    final FaultSkin[] skins, final float[][][] fx, 
-    final float[][][] p2, final float[][][] p3) {
+    final FaultSkin[] skins, final float[][][] fx) {
     final int ns = skins.length;
-    final float[][][][] snd = semblanceNumDen(fx,p2,p3);
+    final float[][][][] snd = semblanceNumDen(fx);
     loop(ns,new LoopInt() {
       public void compute(int is) {
         recomputeLikelihoods(skins[is],snd);
@@ -32,7 +31,7 @@ public class FaultSampleCleaner {
   }
 
   public void recomputeLikelihoods(FaultSkin skin, float[][][][] snd) {
-    int nsmooth = 100;
+    int nsmooth = 50;
     setNumAndDen(skin,snd);
     smoothNumAndDen(nsmooth,skin);
     computeLikelihoods(skin);
@@ -118,110 +117,15 @@ public class FaultSampleCleaner {
     int n3 = f.length;
     int n2 = f[0].length;
     int n1 = f[0][0].length;
-
-    LocalOrientFilter lof = new LocalOrientFilter(8,2,2);
-    EigenTensors ets = lof.applyForTensors(f);
     float[][][] sn = new float[n3][n2][n1];
     float[][][] sd = new float[n3][n2][n1];
-
-    final SincInterpolator si = new SincInterpolator();
-    si.setExtrapolation(SincInterpolator.Extrapolation.CONSTANT);
-    loop(n3,new LoopInt() {
-    public void compute(int i3) {
-      float[] xmm = new float[n1];
-      float[] xm0 = new float[n1];
-      float[] xmp = new float[n1];
-      float[] x0m = new float[n1];
-      float[] x0p = new float[n1];
-      float[] xpm = new float[n1];
-      float[] xp0 = new float[n1];
-      float[] xpp = new float[n1];
-      float[] gmm = new float[n1];
-      float[] gm0 = new float[n1];
-      float[] gmp = new float[n1];
-      float[] g0m = new float[n1];
-      float[] g0p = new float[n1];
-      float[] gpm = new float[n1];
-      float[] gp0 = new float[n1];
-      float[] gpp = new float[n1];
-      int i3m = max(i3-1,0);
-      int i3p = min(i3+1,n3-1);
-      for (int i2=0; i2<n2; ++i2) {
-        int i2m = max(i2-1,0);
-        int i2p = min(i2+1,n2-1);
-        float[] fmm = f[i3m][i2m];
-        float[] fm0 = f[i3m][i2 ];
-        float[] fmp = f[i3m][i2p];
-        float[] f0m = f[i3 ][i2m];
-        float[] f00 = f[i3 ][i2 ];
-        float[] f0p = f[i3 ][i2p];
-        float[] fpm = f[i3p][i2m];
-        float[] fp0 = f[i3p][i2 ];
-        float[] fpp = f[i3p][i2p];
-        float[] p2mm = p2[i3m][i2m];
-        float[] p2mp = p2[i3m][i2p];
-        float[] p20m = p2[i3 ][i2m];
-        float[] p20p = p2[i3 ][i2p];
-        float[] p2pm = p2[i3p][i2m];
-        float[] p2pp = p2[i3p][i2p];
-        float[] p3mm = p3[i3m][i2m];
-        float[] p3m0 = p3[i3m][i2 ];
-        float[] p3mp = p3[i3m][i2p];
-        float[] p3pm = p3[i3p][i2m];
-        float[] p3p0 = p3[i3p][i2 ];
-        float[] p3pp = p3[i3p][i2p];
-        float[] sn32 = sn[i3][i2];
-        float[] sd32 = sd[i3][i2];
-        for (int i1=0; i1<n1; ++i1) {
-          xmm[i1] = i1-p3mm[i1]-p2mm[i1];
-          xm0[i1] = i1-p3m0[i1]         ;
-          xmp[i1] = i1-p3mp[i1]+p2mp[i1];
-          x0m[i1] = i1         -p20m[i1];
-          x0p[i1] = i1         +p20p[i1];
-          xpm[i1] = i1+p3pm[i1]-p2pm[i1];
-          xp0[i1] = i1+p3p0[i1]         ;
-          xpp[i1] = i1+p3pp[i1]+p2pp[i1];
-        }
-        si.interpolate(n1,1.0,0.0,fmm,n1,xmm,gmm);
-        si.interpolate(n1,1.0,0.0,fm0,n1,xm0,gm0);
-        si.interpolate(n1,1.0,0.0,fmp,n1,xmp,gmp);
-        si.interpolate(n1,1.0,0.0,f0m,n1,x0m,g0m);
-        si.interpolate(n1,1.0,0.0,f0p,n1,x0p,g0p);
-        si.interpolate(n1,1.0,0.0,fpm,n1,xpm,gpm);
-        si.interpolate(n1,1.0,0.0,fp0,n1,xp0,gp0);
-        si.interpolate(n1,1.0,0.0,fpp,n1,xpp,gpp);
-        float[] hmm = gmm, hm0 = gm0, hmp = gmp;
-        float[] h0m = g0m, h00 = f00, h0p = g0p;
-        float[] hpm = gpm, hp0 = gp0, hpp = gpp;
-        if (            i2==0   ) h0m = h00;
-        if (            i2==n2-1) h0p = h00;
-        if (i3==0               ) hm0 = h00;
-        if (i3==n3-1            ) hp0 = h00;
-        if (i3==0    && i2==0   ) hmm = h00;
-        if (i3==0    && i2==n2-1) hmp = h00;
-        if (i3==n3-1 && i2==0   ) hpm = h00;
-        if (i3==n3-1 && i2==n2-1) hpp = h00;
-        for (int i1=0; i1<n1; ++i1) {
-          float hmmi = hmm[i1];
-          float hm0i = hm0[i1];
-          float hmpi = hmp[i1];
-          float h0mi = h0m[i1];
-          float h00i = h00[i1];
-          float h0pi = h0p[i1];
-          float hpmi = hpm[i1];
-          float hp0i = hp0[i1];
-          float hppi = hpp[i1];
-          float sumn = hmmi+hm0i+hmpi+
-                       h0mi+h00i+h0pi+
-                       hpmi+hp0i+hppi;
-          float sumd = hmmi*hmmi+hm0i*hm0i+hmpi*hmpi+
-                       h0mi*h0mi+h00i*h00i+h0pi*h0pi+
-                       hpmi*hpmi+hp0i*hp0i+hppi*hppi;
-          sn32[i1] = sumn*sumn;
-          sd32[i1] = 9.0f*sumd;
-        }
-      }
-    }});
+    LocalOrientFilter lof = new LocalOrientFilter(8,2,2);
+    EigenTensors3 ets = lof.applyForTensors(f);
+    ets.setEigenvalues(0.05f,1.0f,1.0f);
+    LocalSmoothingFilter lsf = new LocalSmoothingFilter();
+    lsf.apply(ets,8,f,sn);
+    lsf.apply(ets,8,mul(f,f),sd);
+    mul(sn,sn,sn);
     return new float[][][][]{sn,sd};
   }
 
