5d413063f86e03c6dd55e66fc63910b3aa07a488
xinwucwp <xinwucwp@gmail.com>
2017-07-20 17:11:19 -0500

    update

--- src/crf/utils.py
+++ src/crf/utils.py
@@ -42,6 +42,15 @@ def setupForSubset(name):
     #d1,d2,d3 = 0.002,0.025,0.025 # (s,km,km)
     #f1,f2,f3 = 0.000,5400,10744
     s1,s2,s3 = Sampling(n1,d1,f1),Sampling(n2,d2,f2),Sampling(n3,d3,f3)
+  elif name=="spv":
+    print "setupForDataset: spv"
+    seismicDir = _datdir+"nathan/spv/"
+    n1,n2,n3 = 601,4974,660
+    d1,d2,d3 = 1.0,1.0,1.0 
+    f1,f2,f3 = 0.0,0.0,0.0
+    #d1,d2,d3 = 0.002,0.025,0.025 # (s,km,km)
+    #f1,f2,f3 = 0.000,5400,10744
+    s1,s2,s3 = Sampling(n1,d1,f1),Sampling(n2,d2,f2),Sampling(n3,d3,f3)
   elif name=="nathanSub1":
     print "setupForSubset: nathanSub1"
     seismicDir = _datdir+"nathan/sub1/"
