8901d5cf14b35305adb8d184cec856185e8ec9a1
xinwucwp <xinwucwp@gmail.com>
2017-10-03 15:42:02 -0500

    update

--- src/gu/demoEdge.py
+++ src/gu/demoEdge.py
@@ -63,7 +63,7 @@ maxThrow = 15.0
 # Directory for saved png images. If None, png images will not be saved;
 # otherwise, must create the specified directory before running this script.
 pngDir = None
-plotOnly = False
+plotOnly = True
 #pngDir = "../../png/"
 
 # Processing begins here. When experimenting with one part of this demo, we
